bb61e283bbc884ab4b9192de8cca459fc8ea0b31
Roxana-Ioana <aldearoxanaioana@gmail.com>
2020-03-23 14:48:14 +0200

    update

--- Restaurant/src/main/java/bussinessLayer/RestaurantProcessing.java
+++ Restaurant/src/main/java/bussinessLayer/RestaurantProcessing.java
@@ -7,22 +7,16 @@ import java.util.List;
 public interface RestaurantProcessing {
 
     //Methods for administrator
-    //create new menu item
     void createMenuItem(MenuItem menuItem);
 
-    //edit new menu item
     void editMenuItem(int index, MenuItem editedMenuItem);
 
-    //delete new menu item
     void deleteMenuItem(int index);
 
     //Methods for waiter
-    //create new order
     void createNewOrder(Order order, List<MenuItem> menuItems);
 
-    //compute price
     float computePrice(Order order);
 
-    //generate bill in .txt
     void generateBill(Order order, String fileName);
 }
